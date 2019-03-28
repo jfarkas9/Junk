45ce9d236aa4856e39da78107648e7477b67f591
Josh Farkas <jfarkas09@yahoo.com>
2019-03-28 14:58:21 -0400

    Add project files.

---
+++ Junk/Program.cs
@@ -0,0 +1,23 @@
+﻿using AquaChem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Junk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Stat> Stats = new List<Stat>();
+
+            
+            
+
+        }
+        //List<Stat> stats
+        
+    }
+}
---
+++ Junk/Stat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaChem
+{
+    class Stat
+    {
+
+        public Stat() { }
+
+        public Stat(DateTime date, decimal temperature, decimal pH, decimal ammonia, decimal nitrites, decimal nitrates)
+        {
+            this.Date = date;
+            this.Temperature = temperature;
+            this.PH = pH;
+            this.Ammonia = ammonia;
+            this.Nitrites = nitrites;
+            this.Nitrates = nitrates;
+        }
+
+        public DateTime Date { get; }
+        public decimal Temperature { get; }
+        public decimal PH { get; }
+        public decimal Ammonia { get; }
+        public decimal Nitrites { get; }
+        public decimal Nitrates { get; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Date: {Date}, Temperature: {Temperature}, PH: {PH}, Ammonia: {Ammonia}, Nitrites: {Nitrites}, Nitrates: {Nitrates}";
+        }
+    }
+}
